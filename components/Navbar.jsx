6975888ed975554309f7c479104403e2ad7da657
ElderL99 <adan.lugo.barrientos@outlook.com>
2024-09-08 08:18:12 -0600

    navabar created it is process

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,58 @@
+import { Toaster } from "sonner";
+import Icon from "@/public/Icon";
+
+
 
 export default function Navbar() {
-    const links = [
-        {
-          href: "/",
-          text: "Home",
-        },
-        {
-          href: "/Products",
-          text: "Products",
-        },
-      ];
+  const links = [
+    {
+      href: "/",
+      text: "Home",
+    },
+    {
+      href: "/Products",
+      text: "Products",
+    },
+  ];
 
 
 
-    return (
-        <>
-        <div className="border-4 w-auto h-13" >
-            <nav className="p-2 flex ">
-                <img className=" size-10" src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png" />
-                <input className="border-4 border-gray-300" type="text" name="" id="emai" />
-            </nav>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <header className=" ml-auto border-4 w-auto h-auto flex gap-10">
+        
+        <nav className="ml-80 p-2 flex gap-4 items-center w-full">
+          <img
+            className="size-10"
+            src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+          />
+          <div className="">
+            <form action="/search" className="">
+              {/* Ícono SVG posicionado absolutamente */}
+              <button><Icon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-6 w-6  text-blue-400" /></button>
+              {/* Input con suficiente padding a la izquierda */}
+              <input
+                type="text"
+                name="search"
+                className="pl-12 border rounded h-8 w-80 "
+                placeholder="Search..."
+              />
+            </form>
+          </div>
+          <div className=" ml-80 flex gap-10">
+            <button className="">Login</button>
+            <button className="">Create account</button>
+          </div>
+        </nav>
+      </header>
+
+
+
+
+    </>
+  )
 }
+
+
+
+
