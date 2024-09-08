f9066ad192a121b02399ccca9ae94684f851a3df
ElderL99 <adan.lugo.barrientos@outlook.com>
2024-09-08 09:24:13 -0600

    nueva navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,36 +19,58 @@ export default function Navbar() {
 
   return (
     <>
-      <header className=" ml-auto border-4 w-auto h-auto flex gap-10">
-        
-        <nav className="ml-80 p-2 flex gap-4 items-center w-full">
-          <img
-            className="size-10"
-            src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
-          />
-          <div className="">
-            <form action="/search" className="">
-              {/* Ícono SVG posicionado absolutamente */}
-              <button><Icon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-6 w-6  text-blue-400" /></button>
-              {/* Input con suficiente padding a la izquierda */}
-              <input
-                type="text"
-                name="search"
-                className="pl-12 border rounded h-8 w-80 "
-                placeholder="Search..."
-              />
+      <header className=" border border-gray-300 w-auto ">
+
+        <div className=" flex justify-center items-center ">
+
+          <div className="" id="img">
+            <button href="/">
+            <img className="h-10" src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"  />
+            </button>
+            
+
+          </div>
+
+          <div className=" " id="search">
+            <form action="">
+              <button>
+                <Icon />
+                </button>
+              <input className="bg-gray-400" type="text" name="" id="barra de busqueda" />
             </form>
           </div>
-          <div className=" ml-80 flex gap-10">
-            <button className="">Login</button>
-            <button className="">Create account</button>
+          <div className=" flex gap-8 " id="buttons">
+            
+              <button>Login</button>
+              <button>Create account</button>
+            
           </div>
-        </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
       </header>
-
-
-
-
     </>
   )
 }
