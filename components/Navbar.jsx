ec667ea904afb6632b17c83bff37302b21018efc
ElderL99 <adan.lugo.barrientos@outlook.com>
2024-09-08 19:33:11 -0600

    better navbar styles and chance the form of the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,58 +19,39 @@ export default function Navbar() {
 
   return (
     <>
-      <header className=" border border-gray-300 w-auto ">
-
-        <div className=" flex justify-center items-center ">
-
-          <div className="" id="img">
+      <header className="  w-full bg-white shadow-md py-2 px-4">
+        <div className="flex justify-between items-center max-w-7xl mx-auto  ">
+          <div id="img" className="flex items-center  ">
             <button href="/">
-            <img className="h-10" src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"  />
+              <img
+                className="h-10"
+                src="https://media.dev.to/cdn-cgi/image/quality=100/https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+                alt="DEV logo"
+              />
             </button>
-            
-
           </div>
-
-          <div className=" " id="search">
-            <form action="">
-              <button>
+          <div id="search" className=" flex flex-grow mx-4 border rounded-xl   ">
+            <form action="" className="w-3/4 flex items-center bg-white   ">
+              <button className="p-2">
                 <Icon />
-                </button>
-              <input className="bg-gray-400" type="text" name="" id="barra de busqueda" />
+              </button>
+              <input
+                className="bg-white w-full px-4 py-2 rounded-lg focus:outline-none  hover: border border-blue-500"
+                type="text"
+                id="barra-de-busqueda"
+                placeholder="Search..."
+              />
             </form>
           </div>
-          <div className=" flex gap-8 " id="buttons">
-            
-              <button>Login</button>
-              <button>Create account</button>
-            
+          <div id="buttons" className="flex gap-4">
+            <button className=" text-gray-600 hover:text-blue-500 w-auto ">Login</button>
+            <button className="   text-blue-800 px-4 py-2 rounded-md hover: border border-blue-800">
+              Create account
+            </button>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
-
       </header>
+
     </>
   )
 }
